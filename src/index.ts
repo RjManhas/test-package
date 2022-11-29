4854aef341e75527ed16d04ced580182aa74b3db
Rj Manhas <artaxnetworking@gmail.com>
2022-11-29 10:39:22 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const smartScanner = (name: string) => {
   console.log(`hello ${name}, this program will scan if you are smart!`);
-  const random: number = Math.round(Math.random() *100);
+  const random: number = Math.round(Math.random() * 100);
   console.log(`you are %${random}}`);
   return 1;
 };
