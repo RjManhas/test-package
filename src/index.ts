1f3a375d75fe491a4135370f52bbbacd8f2d5106
Rj Manhas <artaxnetworking@gmail.com>
2022-11-29 10:29:38 -0700

    v2.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+export const smartScanner = (name: string) => {
+  console.log(`hello ${name}, this program will scan if you are smart!`)
+  console.log(`you are %${Math.round(Math.random() *100)}`)
 };
